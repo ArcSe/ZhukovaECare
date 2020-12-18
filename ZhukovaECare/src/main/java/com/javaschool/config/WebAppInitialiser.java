c4557000b7a753bfdb8e6b4611fcf0e3a9823b5c
Zhukova Iuliia <zhukova1998@gmail.com>
2020-12-18 23:06:59 +0300

    maven start setting+config springMVC

---
+++ ZhukovaECare/src/main/java/com/javaschool/config/WebAppInitialiser.java
@@ -0,0 +1,26 @@
+package com.javaschool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+
+public class WebAppInitialiser extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        // TODO Auto-generated method stub
+        return new Class[]{RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        // TODO Auto-generated method stub
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        // TODO Auto-generated method stub
+        return new String[]{"/"};
+    }
+}
---
+++ ZhukovaECare/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Arc
+  Date: 18/12/2020
+  Time: 18:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h2>Hello World!</h2>
+</body>
+</html>
